5d61331d2500411c33553a0afb997da1c24ba731
HubertK2002 <hubistef16@wp.pl>
2025-08-10 16:07:08 +0200

    Dodałem log uruchomienia codeignitera i sprawdzenie czy port uruchomienia jest wolny

--- scripts/run.sh
+++ scripts/run.sh
@@ -33,7 +33,12 @@ if [[ -z "$HOST" || -z "$PORT" ]]; then
 	exit 1
 fi
 
+if lsof -i :$PORT > /dev/null; then
+  echo "Port $PORT jest już zajęty!"
+  exit 1
+fi
+
 cd ../
-sh -c "CI_ENVIRONMENT=$ENVIRONMENT php spark serve --host $HOST --port $PORT > /dev/null 2>&1 & echo \$! > $PID_FILE"
+sh -c "CI_ENVIRONMENT=$ENVIRONMENT php spark serve --host $HOST --port $PORT > ./writable/logs/run_log_$ENV 2>&1 & echo \$! > $PID_FILE"
 
 echo "CodeIgniter ($ENVIRONMENT) uruchomiony na http://$HOST:$PORT (PID zapisany w $PID_FILE)"
